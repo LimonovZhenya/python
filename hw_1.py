378542a049f84f71a1ecd38ee78e3f51aa8f66cc
LimonovZhenya <85179705+LimonovZhenya@users.noreply.github.com>
2021-06-06 22:41:59 +0300

    Add files via upload

---
+++ hw_1.py
@@ -0,0 +1,8 @@
+a = 'hello'
+b = 'world'
+print(f"{a},{b}"
+num1 = int(input('введите число -'))
+num2 = int(input(введите число - ))
+print(f"вы ввели числа{num1} и {num2}")
+world = input ("Enter any word:")
+print(f"{word} отличный выбор!")
